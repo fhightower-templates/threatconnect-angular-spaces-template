b519ff1b983e72e2701fba8048a0a268e008d21c
Floyd Hightower <floyd.hightower27@gmail.com>
2018-04-09 15:51:38 -0400

    Removing debugging and unneeded file
    Fixes #1

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -10,8 +10,6 @@ PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 def replace_escaped_formatting(file_path):
     """Replace escaped jinja formatting in the given file."""
     full_path = os.path.join(PROJECT_DIRECTORY, file_path)
-    print("here: {}".format(file_path))
-    print("here: {}".format(full_path))
 
     with open(full_path, 'r') as f:
         file_text = f.read()
@@ -25,8 +23,6 @@ def replace_escaped_formatting(file_path):
 
 if __name__ == '__main__':
     for path, dirs, files in os.walk("../{{cookiecutter.runtime_prefix}}_-_{{ cookiecutter.project_slug }}/src/app/"):
-        print("here1: {}, {}, {}".format(path, dirs, files))
         for file_ in files:
             if file_.endswith('.html'):
-                print("here: {}, {}, {}".format(path, dirs, files))
                 replace_escaped_formatting(os.path.join(path, file_))
